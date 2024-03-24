a10a5d99bd7f3af7d4fd70657f044ebb05cbb376
picas9dan <picasdan99@gmail.com>
2024-03-24 12:38:01 +0800

    Handle multiple client connections

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
-
 use anyhow::Context;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() -> anyhow::Result<()> {
+async fn handler(socket: &mut TcpStream) {
+    let expected_req = b"*1\r\n$4\r\nping\r\n";
+    let mut remaining_n: usize = 0;
+    loop {
+        let mut req = [0u8; 1024];
+        let buf = b"+PONG\r\n";
+        let n = socket
+            .read(&mut req)
+            .await
+            .context("Read from client")
+            .unwrap();
+        remaining_n += n;
+        if remaining_n >= expected_req.len() {
+            socket
+                .write_all(buf)
+                .await
+                .context("Send PONG response")
+                .unwrap();
+            remaining_n -= expected_req.len();
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .context("List at 127.0.0.1:6379")
+        .unwrap();
 
-    for stream in listener.incoming() {
-        let mut stream = stream.context("Accept TCP connection from client")?;
-        let expected_req = b"*1\r\n$4\r\nping\r\n";
-        let mut remaining_n: usize = 0;
-        loop {
-            let mut req = [0u8; 1024];
-            let buf = b"+PONG\r\n";
-            let n = stream.read(&mut req).context("Received bytes")?;
-            remaining_n += n;
-            if remaining_n >= expected_req.len() {
-                stream.write_all(buf).context("Send PONG response")?;
-                remaining_n -= expected_req.len();
-            }
-        }
+    loop {
+        let (mut socket, _) = listener
+            .accept()
+            .await
+            .context("Accept connection")
+            .unwrap();
+        tokio::spawn(async move { handler(&mut socket).await });
     }
-
-    Ok(())
 }
