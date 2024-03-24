750c282d00a2f4b5157d5958d57b2036afa56407
picas9dan <picasdan99@gmail.com>
2024-03-24 10:23:52 +0800

    Return PONG to client request

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let mut stream = stream.context("Accept TCP connection from client")?;
+        let buf = b"+PONG\r\n";
+        stream.write_all(buf).context("Send PONG response")?;
     }
+
+    Ok(())
 }
